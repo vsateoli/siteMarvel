4e8ce2f24ae1ce0e070de49540f0fc33a327fcc0
Venancio Sa <venanciosateoli@gmail.com>
2019-09-22 13:54:46 -0300

    Funcionando click

--- res/scripts/jogoMemoria.js
+++ res/scripts/jogoMemoria.js
@@ -20,12 +20,13 @@ var imagens = [
     "janeFoster",
     "valquiria",
 ];
-var selecao1, selecao2 = null;
+
+var selecao1 = null;
 
 function OnLoad() {
     if (document.getElementById("jogoMemoria") == null)
         return
-    imagens.sort(function(a, b) { return 0.5 - Math.random() });
+    imagens.sort(function() { return 0.5 - Math.random() });
     for (var i = 0; i < 20; i++) {
         PosicionarImagens(imagens[i], i)
     }
@@ -34,5 +35,19 @@ function OnLoad() {
 function PosicionarImagens(posicao, i) {
     if (jogoMemoria == null || posicao == null || i == null)
         return
-    document.getElementById("jogoMemoria").innerHTML += '<div class="membros ' + i + '"><img src="res/' + imagens[i] + '.jpg"> </div>';
+    document.getElementById("jogoMemoria").innerHTML += '<div class="membros ' + i + '"><a onclick="ClicouImagem(' + i + ')"><img src="res/' + imagens[i] + '.jpg "></a> </div>';
+}
+
+function ClicouImagem(numero) {
+    if (selecao1 == null) {
+        selecao1 = imagens[numero];
+    } else {
+        verificarSimilaridade(numero);
+    }
+}
+
+function verificarSimilaridade(numero) {
+    if (imagens[numero] == selecao1) {
+        alert("acertou");
+    }
 }
